7ca399fa1f7f2a64636c01258e0ec5a0d864b943
Nebojsa Tijanic <boysha@gmail.com>
2014-06-27 16:13:15 +0200

    Trying to include R script with wrapper

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='variant_annotation',
     version='1.0.0',
     packages=find_packages(exclude=['test-data']),
-    install_requires=['sbgsdk']
+    install_requires=['sbgsdk'],
+    include_package_data=True,
+    package_data={
+        'variant_annotation': ['*.R']
+    }
 )
